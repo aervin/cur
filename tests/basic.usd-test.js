d5ad8c3f29d96ce73ae13283b7292175da55b78d
Aaron Ervin <aervin@uste3.com>
2017-09-13 12:44:32 -0400

    negative tests

--- tests/basic.usd-test.js
+++ tests/basic.usd-test.js
@@ -38,6 +38,30 @@ const tests = [
         },
         expected: '9,812.00',
         description: 'It should add zeros if needed...'
+    },
+    {
+        usdParams: {
+            amount: 9812,
+            includeCents: false
+        },
+        expected: '9,812',
+        description: 'It should not add zeros...'
+    },
+    {
+        usdParams: {
+            amount: -9812,
+            includeCents: true
+        },
+        expected: '-9,812.00',
+        description: 'It should add zeros and be negative...'
+    },
+    {
+        usdParams: {
+            amount: -9812,
+            includeCents: false
+        },
+        expected: '-9,812',
+        description: 'It should not add zeros but be negative...'
     }
 ]
 
